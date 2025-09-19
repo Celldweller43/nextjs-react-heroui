320d1db7ad39a596fdbfde107d11ea22955c9fb2
Celldweller43 <nickthepick@msn.com>
2025-09-19 14:14:42 -0500

    asdf

--- app/api/userdata/route.ts
+++ app/api/userdata/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  //const apiKey = process.env.MY_API_KEY;
-  const apiKey = "mck_0q5zlxe3tzycamfhgr7u4s";
+  const apiKey = process.env.MY_API_KEY;
+  //const apiKey = "mck_0q5zlxe3tzycamfhgr7u4s";
 
   const res = await fetch(
     "https://mockfast.io/backend/apitemplate/get/6EFG3H190K",
