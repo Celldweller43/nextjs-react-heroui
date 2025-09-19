bc0d690eaf1f2b0d98190dbcbe926441728e0a03
Celldweller43 <nickthepick@msn.com>
2025-09-19 15:23:37 -0500

    asdf

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -37,39 +37,24 @@ const Test = () => {
       <br />
       <div>
         <div>
-          {Array.isArray(userData) &&
-            userData.map((user: UserType, ndx: number) => (
-              // <div
-              //   key={ndx}
-              //   style={{
-              //     display: "flex",
-              //     flexDirection: "column",
-              //     alignItems: "center",
-              //     justifyContent: "center",
-              //   }}
-              // >
-              //   <div>ID: {user.id}</div>
-              //   <div>Name: {user.name}</div>
-              //   <div>Age: {user.age}</div>
-              //   <div>Email: {user.email}</div>
-              // </div>
-              <Table hideHeader>
-                <TableHeader>
-                  <TableColumn>ID</TableColumn>
-                  <TableColumn>Name</TableColumn>
-                  <TableColumn>Age</TableColumn>
-                  <TableColumn>Email</TableColumn>
-                </TableHeader>
-                <TableBody>
-                  <TableRow key={ndx}>
-                    <TableCell>{user.id}</TableCell>
-                    <TableCell>{user.name}</TableCell>
-                    <TableCell>{user.age}</TableCell>
-                    <TableCell>{user.email}</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            ))}
+          <Table hideHeader>
+            <TableHeader>
+              <TableColumn>ID</TableColumn>
+              <TableColumn>Name</TableColumn>
+              <TableColumn>Age</TableColumn>
+              <TableColumn>Email</TableColumn>
+            </TableHeader>
+            <TableBody>
+              {userData.map((user: UserType, ndx: number) => (
+                <TableRow key={ndx}>
+                  <TableCell>{user.id}</TableCell>
+                  <TableCell>{user.name}</TableCell>
+                  <TableCell>{user.age}</TableCell>
+                  <TableCell>{user.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       </div>
     </div>
